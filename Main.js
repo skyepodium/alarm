05bd20ce168460c78468a7d2d681514327a01608
김정윤 <skyepodium@gmail.com>
2018-01-06 01:21:29 +0900

    01.05

---
+++ Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import Main_text from "./Main_text";
+
+export default class Main extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content"/>
+        <Main_text/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fffaf0',
+  },
+});
---
+++ Main_text.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+export default class Main_text extends Component{
+  handleClick() {
+    console.log('Click happened');
+    Alert.alert("안녕 정윤아~");
+  }
+
+  render(){
+    console.log("asf");
+
+    return(
+      <TouchableOpacity onPress={this.handleClick}>
+        <View>
+          <Text style={styles.ask}>당신의 꿈은 무엇인가요?</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  ask: {
+    fontSize: 60,
+    //fontFamily: "Bmjua"
+    fontFamily: "Bmdohyeon"
+    //fontFamily: "Bmhanna"
+    //fontFamily: "Bmkiranghaerang"
+    //fontFamily: "Bmyeonsung"
+
+  },
+});
